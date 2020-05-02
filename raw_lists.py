e991785d4ab39e272dde86ae5785bcb3ff4f2420
ruru <ruru-git@ruru.pw>
2020-05-02 12:33:25 +0200

    fixed numbering of Roman and Alpha type

--- raw_lists.py
+++ raw_lists.py
@@ -29,9 +29,7 @@ def raw_list(key, value, format, meta):
                         print(typechar[-1])
                         raise ValueError("Wrong formatting")
 
-                    type_of_list = gettype(number_str)
-                    number = int(number_str, 34)
-
+                    type_of_list, number = gettype_and_num(number_str)
 
                     Str_list = []
                     for str_part in listitem_list[1:]:  # loops trough all except the first item,
@@ -49,13 +47,14 @@ def raw_list(key, value, format, meta):
             return OrderedList_list
 
 
-def gettype(list_style_type_character):
+def gettype_and_num(list_style_type_character):
     """ Input typechar of OrderedList, outputs its type
     inputs:  [1-9],   i,          I,          [a-z],      [A-z]
     outputs: Decimal, LowerRoman, UpperRoman, LowerAlpha, UpperAlpha
     """
     if list_style_type_character.isnumeric():
         type = "Decimal"
+        number = int(list_style_type_character, 10)
     elif list_style_type_character.islower():
         type = "Lower"
     elif list_style_type_character.isupper():
@@ -63,13 +62,34 @@ def gettype(list_style_type_character):
     else:
         raise ValueError("Wrong list type")
 
-    if list_style_type_character == "i" or \
-            list_style_type_character == "I":
-        type += "Roma"
+    if checkIfRomanNumeral(list_style_type_character) is None:
+        type += "Roman"
+        number = roman_to_int(list_style_type_character.upper())
     elif not list_style_type_character.isnumeric():
         type += "Alpha"
+        number = int(list_style_type_character, 36) - 9
 
-    return type
+    return type, number
+
+
+def roman_to_int(s):
+    rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    int_val = 0
+    for i in range(len(s)):
+        if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
+            int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+        else:
+            int_val += rom_val[s[i]]
+    return int_val
+
+
+def checkIfRomanNumeral(numeral):
+    """Controls that the userinput only contains valid roman numerals"""
+    numeral = numeral.upper()
+    validRomanNumerals = ["M", "D", "C", "L", "X", "V", "I"]
+    for letters in numeral:
+        if letters not in validRomanNumerals:
+            return False
 
 
 if __name__ == "__main__":
